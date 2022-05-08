8215f21b43274a5feb92c88e928bf3e2d1c51e90
Milu <mks.c@saip.gov.sa>
2022-05-08 12:43:07 +0530

    intial commit

---
+++ src/main/java/com/student/StudentDtailsApplication.java
@@ -0,0 +1,13 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDtailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDtailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Studentdtails/StudentDtailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Studentdtails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDtailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
